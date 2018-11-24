2f22d6071df56eb82ddc299fd1dac91373c565e4
Qingyu Deng <bitdqy@hotmail.com>
2018-11-24 17:57:07 +0800

    add BreadCrumb

---
+++ src/components/ui/BreadCrumb.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="bread-crumb-wrapper">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    seperator: {
+      type: String,
+      default: '·'
+    }
+  },
+  provide () {
+    return {
+      seperator: this.seperator
+    }
+  }
+}
+</script>
+
+<style lang="less">
+.bread-crumb-wrapper {
+  display: flex;
+}
+</style>
+
---
+++ src/components/ui/BreadCrumbItem.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="bread-crumb-item">
+    <slot></slot>
+    <span class="bread-crumb-item-seperator">{{ seperator }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  inject: ['seperator']
+}
+</script>
+
+<style lang="less">
+.bread-crumb-item-seperator {
+  margin: 0 5px;
+}
+
+.bread-crumb-item:last-child {
+  .bread-crumb-item-seperator {
+    display: none;
+  }
+}
+</style>
